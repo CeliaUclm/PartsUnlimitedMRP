3386a493203157fee84161d354b52f6219fc948e
CeliaUclm <celiafernandezsantos@gmail.com>
2021-12-29 20:22:41 +0100

    Segunda tarea

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#no lanzar el .jar hasta que no esté accesible el servicio de mongdb
+#el host 'mongo' tendra una ip otorgada al lanzar la pila de #servicios, 
+#y le damos el apodo 'mongo', utilizado aqui y en el #archivo application.properties
 
 while ! curl http://mongo:27017/
 do
@@ -7,4 +10,4 @@ do
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar -spring.config.location=application.properties
